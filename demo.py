7c1607ef01863762795956f654c561b2133a3e72
AgusCastle <agu13xd@hotmail.com>
2023-04-17 13:18:00 -0600

    Demo

--- demo.py
+++ demo.py
@@ -4,6 +4,12 @@ import torch
 from PIL import Image
 from glob import glob
 from torchvision import transforms
+from pathlib import Path
+from utils.gradcam import viewGradCam, show_cam_on_image
+from models.sparse import SparseFusion
+import numpy as np
+import cv2
+import pdb
 
 def list_models(paths):
     return glob(os.path.join(paths, '*.pth'))
@@ -23,26 +29,108 @@ def demo(device, path_models, path_images):
                                      0.229, 0.224, 0.225])
             ])
 
+    dict_gradcams = {}
     for model in list_models(path_models):
-
+        i = model
         model = torch.load(model, map_location=device)
         model = model['model']
+        model = changeLogSoftmaxbySoftmax(model, str(Path(i).stem))
+
+
         model = model.to(device)
         model.eval()
 
-        
-
         for img in list_images(path_images):
+            key = Path(img).stem
             
+            gradcam = viewGradCam(model, str(Path(i).stem), 1, img, 1)
+            path = img
             img = Image.open(img)
+        
             img = transform(img)
             img = img.to(device)
             img = img[None, :, :, :]
 
             pred = model(img)
 
-            print(torch.exp(pred))
+            ss = pred.tolist()[0]
+            arg = int(torch.argmax(pred))
+            
 
+            print('Model {}, Prediction : {}'.format(str(Path(i).stem), arg))
+
+            if dict_gradcams.get(key) is None:
+                dict_gradcams[key] = { 'softscore': [ss], 'pred': [arg], 'cam' :[gradcam], 'path' : path }
+            else:
+                dict_gradcams.get(key)['softscore'].append(ss)
+                dict_gradcams.get(key)['pred'].append(arg)
+                dict_gradcams.get(key)['cam'].append(gradcam)
+
+    model = torch.load('checkpoints/snfw2.pt', map_location=device)
+    model = model['model']
+    
+    model = model.to(device)
+    model.eval()
+
+    for element in dict_gradcams.values():
+
+        s = element['softscore']
+        s = torch.permute(torch.FloatTensor(s), (1, 0)).to(device)
+        s = s[None, :, :]
+        pred = model(s)
+        pred = int(torch.argmax(pred))
+        print('SNF Prediction: {}'.format(pred))
+        
+        final = []
+        for gradcam, pred_m in zip(element['cam'], element['pred']):
+            if pred_m == pred:
+                final.append(gradcam)
+
+        dst = argmaxPlot(final)
+
+        # dst = None
+        # for i, gray in enumerate(final):    
+        #     if i == 0:
+        #         continue
+        #     if i == 1:
+        #         dst = cv2.addWeighted(final[0], 1, gray, 1, 0.0)
+        #         continue
+        #     dst += cv2.addWeighted(dst, 1, gray, 1, 0.0)
+
+        img = np.array(Image.open(element['path']))
+        img = cv2.resize(img, (512, 512))
+        img = np.float32(img) / 255
+    
+        Image.fromarray(show_cam_on_image(img, cv2.normalize(dst,None, 0, 1, cv2.NORM_MINMAX, dtype=cv2.CV_32F), use_rgb=True)).show()
+        
+def argmaxPlot(list_grayscales):
+
+    final_image = list_grayscales[0]
+
+    for i in range(1, len(list_grayscales)):
+        final_image = sumArgMax(final_image, list_grayscales[i])
+
+    return final_image
+
+def sumArgMax(ndarray1, ndarray2):
+    tmp = np.zeros((512, 512))
+    for i in range(512):
+        for j in range(512):
+            if ndarray1[i, j] > ndarray2[i, j]:
+                tmp[i, j] = ndarray1[i, j]
+            else:
+                tmp[i, j] = ndarray2[i, j]
+    return tmp
+            
+def changeLogSoftmaxbySoftmax(model, flag):
+    if flag == 'convnext_0001':
+        model.attnblocks.fc_[8] = torch.nn.Sequential(torch.nn.Softmax(dim=1))
+    elif flag == 'convnext_0000':
+        model.classifier[10] = torch.nn.Sequential(torch.nn.Softmax(dim=1))
+    else:
+        model.attb.fc_[8] = torch.nn.Sequential(torch.nn.Softmax(dim=1))
+    
+    return model
 demo(1, './checkpoints', './images')
 
 
---
+++ utils/gradcam.py
@@ -0,0 +1,85 @@
+import warnings
+warnings.filterwarnings('ignore')
+import numpy as np
+import cv2
+import requests
+from pytorch_grad_cam import GradCAM
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
+from pytorch_grad_cam.utils.image import show_cam_on_image, \
+    deprocess_image, \
+    preprocess_image
+from PIL import Image
+import torch
+
+
+def viewGradCam(model, tipo:str ,cat: int, img_path: str, device : int = 0):
+    
+    model.eval()
+    img = np.array(Image.open(img_path))
+    img = cv2.resize(img, (512, 512))
+    img = np.float32(img) / 255
+    input_tensor = preprocess_image(img, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+
+    input_tensor = input_tensor.to(device)
+
+    targets = [ClassifierOutputTarget(cat)]
+
+    if tipo == 'convnext_0000':
+        target_layers = [model.features]
+    elif tipo == 'convnext_0001':
+        target_layers = [model.attnblocks.cab_]
+    else:
+        target_layers = [model.attb.cab_]
+    with GradCAM(model=model, target_layers=target_layers) as cam:
+        grayscale_cams = cam(input_tensor=input_tensor, targets=targets)
+        cam_image = show_cam_on_image(img, grayscale_cams[0, :], use_rgb=True)
+    cam = np.uint8(255*grayscale_cams[0, :])
+
+    xcam =np.uint8(255*grayscale_cams[0, :])
+
+    xcam = cv2.merge([cam, cam, cam])
+    images = np.hstack((np.uint8(255*img), xcam , cam_image))
+    
+    if tipo == 'convnext_0000':
+        Image.fromarray(images).show()
+        input()
+    elif tipo == 'convnext_0001':
+        Image.fromarray(images).show()
+        input()
+    # Image.fromarray(cam).show()
+
+    # Image.fromarray(images).show()
+    return cam
+
+def show_cam_on_image(img: np.ndarray,
+                      mask: np.ndarray,
+                      use_rgb: bool = False,
+                      colormap: int = cv2.COLORMAP_JET,
+                      image_weight: float = 0.5) -> np.ndarray:
+    """ This function overlays the cam mask on the image as an heatmap.
+    By default the heatmap is in BGR format.
+
+    :param img: The base image in RGB or BGR format.
+    :param mask: The cam mask.
+    :param use_rgb: Whether to use an RGB or BGR heatmap, this should be set to True if 'img' is in RGB format.
+    :param colormap: The OpenCV colormap to be used.
+    :param image_weight: The final result is image_weight * img + (1-image_weight) * mask.
+    :returns: The default image with the cam overlay.
+    """
+    heatmap = cv2.applyColorMap(np.uint8(255 * mask), colormap)
+    if use_rgb:
+        heatmap = cv2.cvtColor(heatmap, cv2.COLOR_BGR2RGB)
+    heatmap = np.float32(heatmap) / 255
+
+    if np.max(img) > 1:
+        raise Exception(
+            "The input image should np.float32 in the range [0, 1]")
+
+    if image_weight < 0 or image_weight > 1:
+        raise Exception(
+            f"image_weight should be in the range [0, 1].\
+                Got: {image_weight}")
+
+    cam = (1 - image_weight) * heatmap + image_weight * img
+    cam = cam / np.max(cam)
+    return np.uint8(255 * cam)
