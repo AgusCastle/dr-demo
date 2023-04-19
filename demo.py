d616894c982a3bdc3f92b001e815e3f7e0e05ae5
Agus Castillo <agu13xd@hotmail.com>
2023-04-19 17:56:40 -0600

    Fix bugs

--- demo.py
+++ demo.py
@@ -21,14 +21,6 @@ def demo(device, path_models, path_images):
 
     device = torch.device(device)
 
-    transform = transforms.Compose([
-                transforms.Resize(512),
-                transforms.CenterCrop(512),
-                transforms.ToTensor(),
-                transforms.Normalize([0.485, 0.456, 0.406], [
-                                     0.229, 0.224, 0.225])
-            ])
-
     dict_gradcams = {}
     for model in list_models(path_models):
         i = model
